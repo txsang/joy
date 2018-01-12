e1fcbc367cb773ae660a07c159af9c08e66987b2
sangtx <txsang.dn@gmail.com>
2018-01-12 20:41:11 +0700

    feat(About Us): develop page about-us.html

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,23 @@
 wow = new WOW(
-    {
-      animateClass: 'animated',
-      offset:       100,
-      callback:     function(box) {
-        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-      }
-    }
-  );
-  wow.init();
+  {
+    animateClass: 'animated',
+    offset:       100
+  }
+);
+wow.init();
 $(document).ready(function(){
+  $('.btn-menu-mobile').click(function() {
+    $('.btn-menu-mobile').toggleClass('active');
+    $('.header-content').toggleClass('active');
+    $('html, body').toggleClass('active');
+  })
+  // add min height for content
+  let FH = $('.footer-container').height();
+  $('.main-container').css({"min-height": 'calc(100vh - '+ FH + 'px)'});
+})
 
+$(window).resize(function() {
+  let FH = $('.footer-container').height();
+  console.log(FH);
+  $('.main-container').css({"min-height": 'calc(100vh - '+ FH + 'px)'});
 })

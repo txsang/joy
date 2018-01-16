09460b18ffdc953294108f4d59231a2f78376967
sangtx <txsang.dn@gmail.com>
2018-01-16 22:18:38 +0700

    feat(Sign in): develop sign in page

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -14,6 +14,11 @@ $(document).ready(function(){
   // add min height for content
   let FH = $('.footer-container').height();
   $('.main-container').css({"min-height": 'calc(100vh - '+ FH + 'px)'});
+  $('.scroll-down').click(function() {
+    $('html, body').animate({
+        scrollTop: 1000
+      }, 1000)
+  })
 })
 
 $(window).resize(function() {

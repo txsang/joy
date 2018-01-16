93afcf4be052423454991f112497ad202e9da74f
sangtx <txsang.dn@gmail.com>
2018-01-16 23:42:28 +0700

    feat(global): change url file

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,34 +1,27 @@
-wow = new WOW(
-  {
-    animateClass: 'animated',
-    offset:       100
-  }
-);
-
-$(document).ready(function(){
-  $('.btn-menu-mobile').click(function() {
-    $('.btn-menu-mobile').toggleClass('active');
-    $('.header-content').toggleClass('active');
-    $('html, body').toggleClass('active');
-  })
+$(window).ready(function(){
+  $(".btn-menu-mobile").click(function() {
+    $(".btn-menu-mobile").toggleClass("active");
+    $(".header-content").toggleClass("active");
+    $("html, body").toggleClass("active");
+  });
   // add min height for content
-  let FH = $('.footer-container').height();
-  $('.main-container').css({"min-height": 'calc(100vh - '+ FH + 'px)'});
-  $('.scroll-down').click(function() {
-    $('html, body').animate({
+  var FH = $(".footer-container").height();
+  $(".main-container").css({"min-height": "calc(100vh - "+ FH + "px)"});
+  $(".scroll-down").click(function() {
+    $("html, body").animate({
         scrollTop: 1000
-      }, 1000)
-  })
+      }, 1000);
+  });
 })
 
 $(window).resize(function() {
-  let FH = $('.footer-container').height();
-  console.log(FH);
-  $('.main-container').css({"min-height": 'calc(100vh - '+ FH + 'px)'});
+  var FH = $(".footer-container").height();
+  $(".main-container").css({"min-height": "calc(100vh - "+ FH + "px)"});
 })
 
-$(window).on('load', function() {
-  $("html, body").removeClass('loading');
-  $(".loader").addClass('loaded');
+$(window).on("load", function() {
+  $("html, body").removeClass("loading");
+  $(".loader").addClass("loaded");
+  var wow = new WOW({ animateClass: "animated", offset: 100 });
   wow.init();
 });

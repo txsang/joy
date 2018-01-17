737292ce1e3e9ee019002725f68a73f8a361d24a
sangtx <txsang.dn@gmail.com>
2018-01-17 22:20:06 +0700

    feat(layout): responsive layout

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -19,6 +19,15 @@ $(window).resize(function() {
   $(".main-container").css({"min-height": "calc(100vh - "+ FH + "px)"});
 })
 
+$(window).scroll(function () {
+  var scrollTop = $(window).scrollTop();
+  if(scrollTop > 100) {
+    $(".background-header-mobile").css({"opacity": 1});
+  } else {
+    $(".background-header-mobile").css({"opacity": scrollTop / 100});
+  }
+})
+
 $(window).on("load", function() {
   $("html, body").removeClass("loading");
   $(".loader").addClass("loaded");
